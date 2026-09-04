41e745db1a424c4f459a78b2cf68c4cb76f76650
Garvit Arora <garvit@Garvits-MacBook-Air.local>
2026-09-04 14:01:36 +0530

    Fix ReferenceError: useMemo is not defined in ShopContext

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
+import React, { createContext, useContext, useState, useEffect, useMemo } from 'react';
 import { PRODUCTS } from '../data/products';
 import { fetchLiveFlipkartPrice } from '../utils/flipkartPriceScraper';
 
